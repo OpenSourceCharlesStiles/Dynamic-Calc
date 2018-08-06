0289344047d680cab398e3ce9e5f8aa9bfdfdd57
Charles Stiles <41843011+OpenSourceCharlesStiles@users.noreply.github.com>
2018-08-05 19:09:24 -0500

    Add files via upload

---
+++ dynacalc1.0.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+float num1, num2, result;
+
+string userInput;
+
+int main()
+{
+    cout << "Please enter the first value and press enter: ";
+    cin >> num1;
+    cout << "Please enter a function (like +, /, etc) and press enter: ";
+    cin >> userInput;
+    cout << "please enter a second value and press enter: ";
+    cin >> num2;
+
+    if(userInput == "+")
+        {
+        result = num1 + num2;
+        }
+    if(userInput == "/")
+        {
+        result = num1 / num2;
+        }
+    if(userInput == "*")
+        {
+        result = num1 * num2;
+        }
+
+    if(userInput == "-")
+        {
+        result = num1 - num2;
+        }
+
+    if(userInput!= "+" && userInput!= "-" && userInput!= "*" && userInput!= "/")
+        {
+        cerr << "invalid input";
+        }
+
+    else
+        {
+        cout << num1 << userInput << num2 << " equals: " << result << ".\n";
+        }
+
+    return 0;
+    }
